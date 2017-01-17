abb101d0e9786f4dc2a222e312aa9a8153db8273
Jennifer Krejsa <jlkrejsa@gmail.com>
2017-01-17 15:22:40 -0500

    end of day

--- HousesExamples/House.cs
+++ HousesExamples/House.cs
@@ -8,5 +8,23 @@ namespace HousesExamples
 {
     class House
     {
+        private string color;
+        private int rooms;
+        private int size;
+        private int doors;
+
+        public void Paint(string color)
+        {
+            this.color = color;
+        }
+
+        public string CurrentColor()
+        {
+            return color;
+        }
+
+
+
+//end of House class
     }
 }
--- HousesExamples/Program.cs
+++ HousesExamples/Program.cs
@@ -10,6 +10,24 @@ namespace HousesExamples
     {
         static void Main(string[] args)
         {
+            House house1 = new House();
+            // new house object
+            house1.Paint("Green");
+            Console.WriteLine(house1.CurrentColor());
+
+            house1.Paint("White");
+            Console.WriteLine(house1.CurrentColor());
+
+            House house2 = new House();
+            house2.Paint("Blue");
+
+            Console.WriteLine("House one is: " + house1.CurrentColor());
+            Console.WriteLine("House two is: " + house2.CurrentColor());
+
+
+
+
+
         }
     }
 }
